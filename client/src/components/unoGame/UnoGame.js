72d9877838645f138e3b53a31361a05f057c7e4c
Oussama <oussamahamada@yahoo.com>
2021-12-14 09:24:20 +0100

    tinkering some logic

--- client/src/components/unoGame/UnoGame.js
+++ client/src/components/unoGame/UnoGame.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useMemo,
+} from "react";
 import deck from "../../cards/deck.js";
 import colors from "../../cards/colors.ts";
 import numbers from "../../cards/number.ts";
 import types from "../../cards/types.ts";
 import CardImage from "./CardImage.js";
 import arrayShuffle from "array-shuffle";
+import { setTimeout } from "timers";
 const UnoGame = () => {
   let gameDeck = deck.map((el) => el);
+  const [gameStart, setGameStart] = useState(false);
   const [playerOneHand, setPlayerOneHand] = useState([]);
   const [playerTwoHand, setPlayerTwoHand] = useState([]);
   const [cardPile, setCardPile] = useState([]);
   const [pileFirstCard, setPileFirstCard] = useState();
   const [deckPile, setDeckPile] = useState([]);
+  const [playerOneUnoState, setPlayerOneUnoState] = useState(false);
+  const [playerTwoUnoState, setPlayerTwoUnoState] = useState(false);
+  const [failedUnoMessage, setFailedUnoMessage] = useState("");
+  const [turnCount, setTurnCount] = useState(true);
+  const [canDraw, setCanDraw] = useState(true);
+
   useEffect(() => {
     setPlayerOneHand(gameDeck.splice(0, 7));
     setPlayerTwoHand(gameDeck.splice(0, 7));
@@ -27,7 +42,48 @@ const UnoGame = () => {
       }
     }
     setDeckPile(gameDeck);
+    setGameStart(true);
   }, []);
+
+  //detecting winner
+  useEffect(() => {
+    if (gameStart === false) return;
+    switch (true) {
+      case playerOneHand.length === 0:
+        alert("player 1 won the game");
+        return;
+      case playerTwoHand.length === 0:
+        alert("player 2 won the game");
+        return;
+      default:
+        return;
+    }
+  }, [playerOneHand, playerTwoHand, gameStart]);
+
+  //detecting Empty deck & suffling with card pile
+
+  const mergeDeckWithPile = useCallback(() => {
+    if (!gameStart) return;
+    if (deckPile.length > 6) return;
+    if (cardPile.length === 1) return;
+    setDeckPile(arrayShuffle(cardPile.slice(0, cardPile.length - 1)));
+    setCardPile([pileFirstCard]);
+  }, [deckPile, pileFirstCard, gameStart, cardPile]);
+
+  useLayoutEffect(() => {
+    mergeDeckWithPile();
+  }, [mergeDeckWithPile]);
+
+  useEffect(() => {
+    let tast = true;
+    for (let i of playerTwoHand) {
+      if (playCard(i, 2, true)) {
+        playCard(i, 2, false);
+        return;
+      }
+    }
+  }, [playerOneHand]);
+
   const cardPlayingLogicHandling = (card, player, newColor) => {
     setCardPile((prevState) => [...prevState, card]);
     !newColor
@@ -37,94 +93,45 @@ const UnoGame = () => {
       ? setPlayerOneHand((prevState) => prevState.filter((el) => el !== card))
       : setPlayerTwoHand((prevState) => prevState.filter((el) => el !== card));
   };
+
   const cardDrawLogicHandling = (player, numberOfCardsToDraw) => {
     if (player === 1) {
-      switch (numberOfCardsToDraw) {
-        case 1:
-          setPlayerTwoHand((prevState) => [...prevState, deckPile[0]]);
-          break;
-        case 2:
-          setPlayerTwoHand((prevState) => [
-            ...prevState,
-            deckPile[0],
-            deckPile[1],
-          ]);
-          break;
-        case 4:
-          setPlayerTwoHand((prevState) => [
-            ...prevState,
-            deckPile[0],
-            deckPile[1],
-            deckPile[2],
-            deckPile[3],
-          ]);
-          break;
-        case 6:
-          setPlayerTwoHand((prevState) => [
-            ...prevState,
-            deckPile[0],
-            deckPile[1],
-            deckPile[2],
-            deckPile[3],
-            deckPile[4],
-            deckPile[5],
-          ]);
-          break;
-        default:
-          break;
-      }
-    } else {
-      switch (numberOfCardsToDraw) {
-        case 1:
-          setPlayerTwoHand((prevState) => [...prevState, deckPile[0]]);
-          break;
-        case 2:
-          setPlayerTwoHand((prevState) => [
-            ...prevState,
-            deckPile[0],
-            deckPile[1],
-          ]);
-          break;
-        case 4:
-          setPlayerTwoHand((prevState) => [
-            ...prevState,
-            deckPile[0],
-            deckPile[1],
-            deckPile[2],
-            deckPile[3],
-          ]);
-          break;
-        case 6:
-          setPlayerTwoHand((prevState) => [
-            ...prevState,
-            deckPile[0],
-            deckPile[1],
-            deckPile[2],
-            deckPile[3],
-            deckPile[4],
-            deckPile[5],
-          ]);
-          break;
-        default:
-          break;
-      }
+      setPlayerTwoHand((prevState) => [
+        ...prevState,
+        ...deckPile.slice(0, numberOfCardsToDraw),
+      ]);
+    } else if (player === 2) {
+      setPlayerOneHand((prevState) => [
+        ...prevState,
+        ...deckPile.slice(0, numberOfCardsToDraw),
+      ]);
     }
     setDeckPile((prevState) =>
       prevState.filter((el, index) => index > numberOfCardsToDraw - 1)
     );
   };
+
   const pileDrawlogicHandling = (player) => {
+    if ((player === 1 && turnCount) || (player === 2 && !turnCount)) return;
     cardDrawLogicHandling(player, 1);
+    setTurnCount((prevState) => !prevState);
   };
-  const playCard = (card, player) => {
+
+  const playCard = (card, player, test) => {
     let newColor;
+    let isCardPlayable = false;
+    if ((player === 2 && turnCount) || (player === 1 && !turnCount))
+      return isCardPlayable;
     switch (card.type) {
       case types.NORMAL:
         if (
           card.color === pileFirstCard.color ||
           card.number === pileFirstCard.number
         ) {
+          isCardPlayable = true;
+          if (test) break;
           cardPlayingLogicHandling(card, player);
+          setTurnCount((prevState) => !prevState);
         }
         break;
       case types.SPECIAL:
@@ -132,41 +139,123 @@ const UnoGame = () => {
           card.color === pileFirstCard.color ||
           card.number === pileFirstCard.number
         ) {
+          isCardPlayable = true;
+          if (test) break;
           cardPlayingLogicHandling(card, player);
+
           if (card.type === types.SPECIAL && card.number === numbers.DRAW_TWO) {
             cardDrawLogicHandling(player, 2);
           }
         }
         break;
       case types.WILD:
-        newColor = prompt("choose color");
+        isCardPlayable = true;
+        if (test) break;
+        // newColor = prompt("choose color");
+        newColor = "red";
         cardPlayingLogicHandling(card, player, newColor);
+        setTurnCount((prevState) => !prevState);
+
         break;
       case types.WILD_FOUR:
-        newColor = prompt("choose color");
+        isCardPlayable = true;
+        if (test) break;
+        // newColor = prompt("choose color");
+        newColor = "red";
         cardPlayingLogicHandling(card, player, newColor);
-        if (card.type === types.WILD_FOUR) {
-          cardDrawLogicHandling(player, 4);
-        }
+        cardDrawLogicHandling(player, 4);
+
         break;
       default:
-        alert("no");
+        return;
+    }
+    if (
+      player === 1 &&
+      playerOneHand.length === 2 &&
+      !playerOneUnoState &&
+      isCardPlayable
+    ) {
+      if (test) return isCardPlayable;
+      setFailedUnoMessage(
+        `sorry player ${player} , you forgot to say uno, take a small gift`
+      );
+      setTimeout(() => setFailedUnoMessage(""), 10000);
+      cardDrawLogicHandling(2, 2);
+    }
+    if (
+      player === 2 &&
+      playerTwoHand.length === 2 &&
+      !playerTwoUnoState &&
+      isCardPlayable
+    ) {
+      if (test) return isCardPlayable;
+      setFailedUnoMessage(
+        `sorry player ${player} , you forgot to say uno, take a small gift`
+      );
+      setTimeout(() => setFailedUnoMessage(""), 10000);
+      cardDrawLogicHandling(1, 2);
+    }
+    if (!test) {
+      setPlayerOneUnoState(false);
+      setPlayerTwoUnoState(false);
+    }
+    return isCardPlayable;
+  };
+
+  const unoButtonLogicHandling = (player) => {
+    let isTherePlayableCardInHand = false;
+    switch (player) {
+      case 1:
+        if (playerOneHand.length !== 2) return;
+        for (let i of playerOneHand) {
+          if (playCard(i, 1, true)) {
+            isTherePlayableCardInHand = true;
+            break;
+          }
+        }
+        if (!isTherePlayableCardInHand) return;
+        alert(`player ${player} annouced uno`);
+        setPlayerOneUnoState(true);
+        break;
+      case 2:
+        if (playerTwoHand.length !== 2) return;
+        for (let i of playerTwoHand) {
+          if (playCard(i, 2, true)) {
+            isTherePlayableCardInHand = true;
+            break;
+          }
+        }
+        if (!isTherePlayableCardInHand) return;
+        alert(`player ${player} annouced uno`);
+        setPlayerTwoUnoState(true);
+        break;
+      default:
+        return;
     }
   };
+
   return (
     <div>
+      <p>
+        1:{playerOneUnoState.toString()} 2: {playerTwoUnoState.toString()}
+      </p>
+      {failedUnoMessage && <p>{failedUnoMessage}</p>}
       <p>player 2 hand</p>
       <button onClick={pileDrawlogicHandling.bind(this, 1)}>DRAW</button>
+      <button onClick={unoButtonLogicHandling.bind(this, 2)}>UNO</button>
+      {/* {!turnCount && <span>Mine trun</span>} */}
       {playerTwoHand.map((card) => (
-        <span onClick={playCard.bind(this, card, 2)}>
+        <span onClick={playCard.bind(this, card, 2, false)}>
           <CardImage card={card} />
         </span>
       ))}
 
       <p>player 1 hand</p>
       <button onClick={pileDrawlogicHandling.bind(this, 2)}>DRAW</button>
+      <button onClick={unoButtonLogicHandling.bind(this, 1)}>UNO</button>
+      {/* {turnCount && <span>Mine trun</span>} */}
       {playerOneHand.map((card) => (
-        <span onClick={playCard.bind(this, card, 1)}>
+        <span onClick={playCard.bind(this, card, 1, false)}>
           <CardImage card={card} />
         </span>
       ))}
@@ -199,3 +288,10 @@ const UnoGame = () => {
 };
 
 export default UnoGame;
+
+/*missing features 
+//reshuffle pile
+//cant draw if its not your turn
+handle deck pile draw error (drawing more than rest in deckpile)
++4 challenge
+*/
